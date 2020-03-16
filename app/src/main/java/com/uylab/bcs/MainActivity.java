49338f2533713d7d1324f7b81f123b96467c4349
faisal-hub <faisalahmedf19@gmail.com>
2020-03-16 12:02:06 +0600

    ok

---
+++ app/src/main/java/com/uylab/bcs/MainActivity.java
@@ -0,0 +1,14 @@
+package com.uylab.bcs;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
